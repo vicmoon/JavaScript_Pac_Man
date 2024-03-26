8a55e83b03fdb6c80ea48bb6b95447003c757ff4
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2024-03-26 20:41:59 +0100

    add main function

--- script.js
+++ script.js
@@ -47,7 +47,8 @@ document.addEventListener('DOMContentLoaded', ()=> {
     const squares = []; 
     function createBoard() {
         for (let i = 0; i < layout.length; i++) {
-          const square = document.createElement('div')
+          const square = document.createElement('div'); 
+          square.id = i; 
           grid.appendChild(square)
           squares.push(square)
       
@@ -63,8 +64,102 @@ document.addEventListener('DOMContentLoaded', ()=> {
           }
         }
       }
-      createBoard()
-      
+      createBoard();
+
+
+
+//create character 
+
+//draw pac-man onto the board 
+
+let pacmanCurrentIndex = 490;
+squares[pacmanCurrentIndex].classList.add('pac-man');
+
+// move pacman 
+
+function movePacMan(e){
+    squares[pacmanCurrentIndex].classList.remove('pac-man');
+
+switch(e.key){
+    case 'ArrowLeft':
+        if(pacmanCurrentIndex % width !==0 && 
+            !squares[pacmanCurrentIndex -1].classList.contains('wall') &&
+            !squares[pacmanCurrentIndex -1].classList.contains('ghost-lair') )
+         {
+           pacmanCurrentIndex -=1;
+        }
+        if(squares[pacmanCurrentIndex - 1] === squares[363]){
+            pacmanCurrentIndex = 391; 
+        }
+        break;
+   case 'ArrowUp':
+       if(pacmanCurrentIndex - width >= 0 &&
+        !squares[pacmanCurrentIndex - width].classList.contains('wall')&&
+        !squares[pacmanCurrentIndex -1].classList.contains('ghost-lair')){
+        pacmanCurrentIndex -= width;
+       }
+
+        break;
+    case 'ArrowRight':
+        if(pacmanCurrentIndex % width < width -1 && 
+            !squares[pacmanCurrentIndex +1].classList.contains('wall') &&
+            !squares[pacmanCurrentIndex +1].classList.contains('ghost-lair') )
+        {
+            pacmanCurrentIndex +=1;
+        }if(squares[pacmanCurrentIndex + 1 ] === squares[392]){
+            pacmanCurrentIndex = 364; 
+        }
+        break;   
+
+    case 'ArrowDown':
+        if(pacmanCurrentIndex + width < width * width &&
+            !squares[pacmanCurrentIndex +1].classList.contains('wall') &&
+            !squares[pacmanCurrentIndex +1].classList.contains('ghost-lair') 
+             ){
+            pacmanCurrentIndex += width;
+        }
+        break;
+}
+squares[pacmanCurrentIndex].classList.add('pac-man');
+
+pacDotEaten();
+powerPelletEater(); 
+checkForGameOver();
+// checkForWin();
+
+}
+document.addEventListener('keyup', movePacMan);
+
+
+
+// when a pac-dot is eaten 
+
+function pacDotEaten(){
+    if(squares[pacmanCurrentIndex].classList.contains('pac-dot')){
+       score++; 
+       scoreDisplay.innerHTML = score;
+       squares[pacmanCurrentIndex].classList.remove('pac-dot'); 
+    }
+
+};
+
+// when a pellet eater is eaten 
+
+function powerPelletEater(){
+    if(squares[pacmanCurrentIndex].classList.contains('power-pellet')){
+       score += 10;
+       scoreDisplay.innerHTML = score;
+       squares[pacmanCurrentIndex].classList.remove('power-pellet')
+    }
+
+}
+
+
+function checkForGameOver(){
+    
+}
+
+
 
 
 });
